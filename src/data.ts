ff7a80291c9f1520baa3a6e0e2baa056c2c68674
Pedro <pedroardo10@gmail.com>
2025-10-14 23:58:28 +0700

    Make the homepage

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,8 @@
 import LoaderGambar1 from "../public/20231022_141734.jpg";
 import LoaderGambar2 from "../public/20231029_195905(0).jpg";
-
+import LoaderGambar3 from "../public/img1.jpg";
+import LoaderGambar4 from "../public/img2.jpg";
+import LoaderGambar5 from "../public/img3.jpg";
 export const LoadingGambar = [
   {
     id: 1,
@@ -9,5 +11,17 @@ export const LoadingGambar = [
   {
     id: 2,
     quote: LoaderGambar2,
+  },
+  {
+    id: 3,
+    quote: LoaderGambar3,
+  },
+  {
+    id: 4,
+    quote: LoaderGambar4,
+  },
+  {
+    id: 5,
+    quote: LoaderGambar5,
   },
 ];
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 function HomePage() {
+  const [translate, setTranslate] = useState({ x: 0, y: 0 });
+
+  const moveButton = () => {
+    const randomX = Math.floor(Math.random() * 200 - 100);
+    const randomY = Math.floor(Math.random() * 200 - 100);
+    setTranslate({ x: randomX, y: randomY });
+  };
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="h-lvh flex flex-col justify-center">
+      <div className="items-center justify-center flex flex-col font-display mx-auto">
+        <img src="../../public/1.gif" alt="Cute Icon" className="size-52" />
+        <h1 className="font-bold text-pink-500 text-[1.8rem] text-center">
+          happy birthday my girlfriend!!
+        </h1>
+
+        <div className="flex flex-col gap-2 my-4 text-center items-center justify-center relative">
+          <p className=" text-pink-600 text-lg">Do You Love Me?</p>
+          <div className="flex gap-2">
+            <button className="border-2  text-white rounded-lg py-2 px-8 hover:bg-green-600 transition-all bg-green-400">
+              YES!!!
+            </button>
+            <button
+              className="border-1 rounded-lg py-2 px-8 transition-transform duration-300 border-black bg-red-500 text-white"
+              style={{
+                transform: `translate(${translate.x}px, ${translate.y}px)`,
+              }}
+              onMouseEnter={moveButton}
+            >
+              NO!!!
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
