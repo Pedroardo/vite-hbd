60026b889c21fedcc77818d00d02b68a8278b0b4
Pedro <pedroardo10@gmail.com>
2025-10-16 20:02:31 +0700

    add draggable card + music

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,40 @@
 import { useState } from "react";
+import { FlipWords } from "../components/ui/flip-words";
+import MainPage from "./MainPage";
 
 function HomePage() {
   const [translate, setTranslate] = useState({ x: 0, y: 0 });
-
+  const [showComponent, setShowComponent] = useState(false);
   const moveButton = () => {
     const randomX = Math.floor(Math.random() * 200 - 100);
     const randomY = Math.floor(Math.random() * 200 - 100);
     setTranslate({ x: randomX, y: randomY });
   };
+  if (showComponent) {
+    return <MainPage />;
+  }
 
   return (
     <div className="h-lvh flex flex-col justify-center">
       <div className="items-center justify-center flex flex-col font-display mx-auto">
         <img src="../../public/1.gif" alt="Cute Icon" className="size-52" />
         <h1 className="font-bold text-pink-500 text-[1.8rem] text-center">
-          happy birthday my girlfriend!!
+          happy birthday <br /> my
+          <FlipWords
+            words={["girlfriend", "love", "half", "only one"]}
+            className="text-pink-500"
+          />
         </h1>
-
         <div className="flex flex-col gap-2 my-4 text-center items-center justify-center relative">
           <p className=" text-pink-600 text-lg">Do You Love Me?</p>
           <div className="flex gap-2">
-            <button className="border-2  text-white rounded-lg py-2 px-8 hover:bg-green-600 transition-all bg-green-400">
+            <button
+              className="border-2  text-white rounded-lg py-2 px-8 hover:bg-green-600 transition-all bg-green-400"
+              onClick={() => setShowComponent(true)}
+            >
               YES!!!
             </button>
+            {showComponent && <MainPage />}
             <button
               className="border-1 rounded-lg py-2 px-8 transition-transform duration-300 border-black bg-red-500 text-white"
               style={{
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  DraggableCardBody,
+  DraggableCardContainer,
+} from "../components/ui/draggable-card";
+import { DraggableImg } from "../data";
+function MainPage() {
+  return (
+    <div>
+      <audio src="../../public/fix-you.mp3" autoPlay loop></audio>
+      <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
+        <p className="absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-500 md:text-4xl dark:text-neutral-800">
+          every year, you grow wiser, kinder, and even more amazing. happy
+          birthday, my girl ❤️
+        </p>
+        
+        {DraggableImg.map((item) => (
+          <DraggableCardBody className={item.className}>
+            <img
+              src={item.image}
+              alt={item.title}
+              className="pointer-events-none relative z-10 h-80 w-80 object-cover"
+            />
+            <h3 className="mt-4 text-center text-2xl font-bold text-neutral-700 dark:text-neutral-300">
+              {item.title}
+            </h3>
+          </DraggableCardBody>
+        ))}
+      </DraggableCardContainer>
+    </div>
+  );
+}
+
+export default MainPage;
